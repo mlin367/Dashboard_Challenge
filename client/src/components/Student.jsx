7ece6314f3e7fb9440053e0422d8b6a822ba01cc
Matthew Lin <mlin367@yahoo.com>
2019-06-12 22:13:57 -0700

    changed two two decimal places and changed step to .01

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Student = props => (
   <div className="student">
     <h2 className="name">Name: {props.student.firstName} {props.student.lastName}</h2>
-    <h3 className="attenPer">Absent Percentage: {100 - props.student.attendancePercentage}%</h3>
+    <h3 className="attenPer">Absent Percentage: {(100 - props.student.attendancePercentage).toFixed(2)}%</h3>
     <div className="attenPer">Attendance Percentage: {props.student.attendancePercentage}%</div>
     <div className="grade">Grade: {props.student.grade}</div>
     <div className="cohort">Cohort: {props.student.cohort}</div>
