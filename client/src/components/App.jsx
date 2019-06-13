1c2697fbceff14784b3124efba0dc2551e6d517f
Matthew Lin <mlin367@yahoo.com>
2019-06-12 20:15:52 -0700

    slider and input box working as intended

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import Student from './Student';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      threshold: '0'
     };
   }
 
   render() {
     return (
       <div className="app">
-        
+        <input onChange={(e) => this.setState({ threshold: e.target.value })} type="number" min="0" max="100" value={this.state.threshold} />
+        <input className="slider" onChange={(e) => this.setState({ threshold: e.target.value })} type="range" min="0.00" max="100.00" value={this.state.threshold} step="0.01"  />
+        <h3>{this.state.threshold}</h3>
       </div>
     );
   }
